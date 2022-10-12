2238debfa9e4fc110299701e7d7e9e3c92de07ed
Theodoric-Graham <theodoricgraham@hotmail.com>
2022-10-11 19:08:28 -0500

    created input container

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Practice Project </h1>
+      <div className="input-container">
+        <strong>Username</strong>
+        <input></input>
+        <strong>Age (Years)</strong>
+        <input></input>
+        <button>Add User</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
